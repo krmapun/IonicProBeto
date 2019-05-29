66e2e03636dca48ea5c263973a9e1f4eb7f3ebef
Manuel Alberto Rojas Martinez <krmapun@gmail.com>
2019-05-29 16:04:58 -0500

    commit
    holaword

--- appCroNex/src/app/pages/reportes/reportes.page.ts
+++ appCroNex/src/app/pages/reportes/reportes.page.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../servicios/auth.service';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
 import { Observable } from 'rxjs';
+import { TaskI } from '../../models/task.interface';
+import { map } from 'rxjs/operators';
+import { OrderBy } from '../../../../../IonFire/src/app/orderby.pipe';
 
 @Component({
   selector: 'app-reportes',
@@ -12,16 +15,21 @@ export class ReportesPage implements OnInit {
 
   items: Observable<any[]>;
   tasks: Observable<any[]>;
+  binding: number;
+
   constructor(public authservice: AuthService,
-                      db: AngularFirestore)
+              public db: AngularFirestore)
   {
+
   const usuid = window.localStorage.getItem('idusu');
   this.items = db.collection('todos', ref => ref.where('usuario', '==', usuid)).valueChanges();
   this.tasks = db.collection('calcular',ref => ref.where('usuario', '==', usuid).limit(1)).valueChanges();
   }
 
   ngOnInit() {
-    console.log(this.items);
+    const usuid = window.localStorage.getItem('idusu');
+    var refer = this.db.collection('todos', ref => ref.where('usuario', '==', usuid)).get().subscribe(res => console.log(res) ) ;
+    console.log(refer);
   }
 
   signOut() {
